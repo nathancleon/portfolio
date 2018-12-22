5f3d6b4f111d4a4279a1bb29b71bd59457a1113f
Nathaniel Collins <nathancleon@gmail.com>
2018-12-21 21:13:55 -0500

    projects section updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import mentalnoteImg from '../../images/mentalnotemockup@2x.png'
-import imgOfRenly from '../../images/renly-color.png'
-import imgOfMyself from '../../images/me-color.png'
+import mentalnoteImg from '../../images/mentalnotemockup.png'
+import sproutlogImg from '../../images/sproutlog-mockup.png'
+import barklocalImg from '../../images/barklocal-mockup.png'
+import htmlIcon from '../../images/icons/html-5.svg'
+import cssIcon from '../../images/icons/css-3.svg'
+import jqueryIcon from '../../images/icons/jquery.svg'
+import nodeJSIcon from '../../images/icons/nodejs-icon.svg'
+import reactIcon from '../../images/icons/react.svg'
+import reduxIcon from '../../images/icons/redux.svg'
+import jestIcon from '../../images/icons/jest.svg'
+import mochaIcon from '../../images/icons/mocha.svg'
+import chaiIcon from '../../images/icons/chai.svg'
+import githubIcon from '../../images/icons/github-icon.svg'
 
 const ProjectsContainer = styled.div`
   {
     display: flex;
     flex-direction: column;
     align-items: center;
-    height: 110vh;
-    width: 100vw;
     padding-top: 15vh;
     background-color: #fefefe;
   }
@@ -19,17 +27,18 @@ const ProjectsContainer = styled.div`
 const ContentContainer = styled.div`
   {
     display: flex;
-    align-items: center;
-    justify-content: space-around;
-    height: 100%;
+    align-items: top;
+    justify-content: space-between;
+    height: 80%;
     width: 100%;
+    padding-left: 5vw;
   }
 `;
 
 const Title = styled.h1`
   {
     font-family: 'Merriweather', serif;
-    font-size: 5vmin;
+    font-size: 4vmin;
     color: #fefefe;
     position: relative;
     transform: rotate(-5deg);
@@ -38,9 +47,9 @@ const Title = styled.h1`
   &:before {
     content: '';
     position: absolute;
-    width: 23vmin;
+    width: 20vmin;
     height: 8vmin;
-    top: -1vmin;
+    top: -2vmin;
     left: -2.5vmin;
     background-color: #715FB5;
     border-radius: 4px;
@@ -49,10 +58,10 @@ const Title = styled.h1`
   &:after {
     content: '';
     position: absolute;
-    width: 23vmin;
+    width: 24vmin;
     height: 8vmin;
     top: -1.5vmin;
-    left: -2vmin;
+    left: -4vmin;
     background-color: #8DE4F5;
     border-radius: 4px;
     z-index: -2;
@@ -74,9 +83,6 @@ const ProjectList = styled.ul`
     margin: 0;
     margin-left: -1%;
   }
-  &:first-child {
-    margin-top: 20%;
-  }
   li {
     margin: 0;
     width: 100%;
@@ -96,37 +102,42 @@ const ProjectList = styled.ul`
     height: 1px;
     background-color: #eee;
   }
-  li:hover {
-    background-color: #f8f8f8;
-  }
   li span {
-    font-size: 1.5rem;
+    font-size: 1.125rem;
     font-family: "Open Sans", sans-serif;
     font-weight: bold;
     color: #715FB5;
   }
   li p {
-    font-size: 1rem;
+    font-size: 0.75rem;
     font-family: "Open Sans", sans-serif;
+    color: #888;
   }
 `;
 
 const SelectedProject = styled.div`
   {
-    width: 70%;
+    height: 100%;
+    width: 85%;
     display: flex;
     flex-direction: column;
     align-items: center;
   }
   img {
+    width: 80%;
   }
 `;
 
 const TechStack = styled.div`
   {
-    width: 60%;
+    width: 50%;
     display: flex;
     justify-content: space-between;
+    margin-top: -5vh;
+  }
+  img {
+    width: 3vw;
+    max-height: 3vw;
   }
 `;
 
@@ -137,27 +148,106 @@ export default class Projects extends React.Component {
     this.state = {
       projects: [
         {
-          title: "mentalnote",
-          description: "an online mental health journal",
-          techStack: ["react", "node", "mocha", "chai", "enzyme", "github"],
+          title: "Mentalnote",
+          description: "An online mental health journal",
+          techStack: [
+            {
+              icon: reactIcon,
+              altText: 'react'
+            },
+            {
+              icon: reduxIcon,
+              altText: 'redux'
+            },
+            {
+              icon: nodeJSIcon,
+              altText: 'node'
+            },
+            {
+              icon: mochaIcon,
+              altText: 'mocha'
+            },
+            {
+              icon: chaiIcon,
+              altText: 'chai'
+            },
+            {
+              icon: jestIcon,
+              altText: 'jest'
+            },
+            {
+              icon: githubIcon,
+              altText: 'github',
+              link: ''
+            }
+          ],
           image: mentalnoteImg
         },
         {
-          title: "mentalnote",
-          description: "an online mental health journal",
-          techStack: ["react", "node", "mocha", "chai", "enzyme", "github"],
-          image: imgOfMyself
+          title: "Sprout Log",
+          description: "An application to track your plant's health",
+          techStack: [
+            {
+              icon: jqueryIcon,
+              altText: 'jQuery'
+            },
+            {
+              icon: nodeJSIcon,
+              altText: 'node'
+            },
+            {
+              icon: mochaIcon,
+              altText: 'mocha'
+            },
+            {
+              icon: chaiIcon,
+              altText: 'chai'
+            },
+            {
+              icon: githubIcon,
+              altText: 'github',
+              link: ''
+            }
+          ],
+          image: sproutlogImg
         },
         {
-          title: "mentalnote",
-          description: "an online mental health journal",
-          techStack: ["react", "node", "mocha", "chai", "enzyme", "github"],
-          image: imgOfRenly
+          title: "Bark Local",
+          description: "Enter your zipcode local dog parks using Google Maps & Foursquare",
+          techStack: [
+            {
+              icon: htmlIcon,
+              altText: 'react'
+            },
+            {
+              icon: cssIcon,
+              altText: 'css'
+            },
+            {
+              icon: jqueryIcon,
+              altText: 'jQuery'
+            },
+            {
+              icon: githubIcon,
+              altText: 'github',
+              link: ''
+            }
+          ],
+          image: barklocalImg
         },
       ],
-      selected: null
+      selectedIndex: 0
     }
+
+    this.selectedProject = this.selectedProject.bind(this);
   }
+
+  selectedProject(index) {
+      this.setState({
+        selectedIndex: index
+      })
+  }
+
   render() {
     return(
       <ProjectsContainer>
@@ -165,10 +255,10 @@ export default class Projects extends React.Component {
         <ContentContainer>
           <ProjectListContainer>
             {
-            this.state.projects.map(project => {
+            this.state.projects.map((project, index) => {
               return (
-                <ProjectList>
-                  <li>
+                <ProjectList key={index}>
+                  <li onClick={() => this.selectedProject(index)}>
                     <span>{project.title}</span>
                     <p>{project.description}</p>
                   </li>
@@ -178,12 +268,12 @@ export default class Projects extends React.Component {
             }
           </ProjectListContainer>
         <SelectedProject>
-          <img src={this.state.projects[0].image} alt="design mockup" />
+          <img src={this.state.projects[this.state.selectedIndex].image} alt="design mockup" />
           <TechStack>
           {
-            this.state.projects[0].techStack.map(stack => {
+            this.state.projects[this.state.selectedIndex].techStack.map((stack, index) => {
               return (
-                <span>{stack}</span>
+                <img key={index} src={stack.icon} alt={stack.altText} title={stack.altText} />
               )
             })
           }
