ba98cc63d63c87a90b007aa177ae3e59540122ad
Nathaniel Collins <nathancleon@gmail.com>
2019-09-23 20:11:51 -0400

    minor fix

--- src/components/seo.js
+++ src/components/seo.js
@@ -60,7 +60,7 @@ function SEO({ description, lang, meta, keywords, title }) {
               },
               {
                 property: 'twitter:image',
-                content: `https://www.nathancleon.com/${WebsiteScreenshot}`,
+                content: `https://www.nathancleon.com${WebsiteScreenshot}`,
               },
             ]
               .concat(
